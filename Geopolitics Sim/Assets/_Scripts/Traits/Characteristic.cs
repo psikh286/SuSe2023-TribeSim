8f31b8e69d66205b38f50af8f07d05cfa2c6825f
Alan Berberov <alan.almaco67@gmail.com>
2023-05-24 09:02:59 +0200

    [ADD] a sample of characteristic;

---
+++ Geopolitics Sim/Assets/_Scripts/Traits/Characteristic.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class TMotivation
+{
+    [SerializeField] private float Value;
+    
+    public float Evaluate(float b)
+    {
+        var a = Value;
+
+        return (a + b) * 0.5f;
+    }
+}
