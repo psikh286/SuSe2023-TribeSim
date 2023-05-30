d84acb3f9310f12492d126fb59a002a31c592c8e
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 01:06:49 +0200

    [ADD] Energy task nodes;

---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/Tasks/Energy/TaskRest.cs
@@ -0,0 +1,17 @@
+﻿using BehaviorTree;
+
+public class TaskRest : Node
+{
+    public TaskRest(FoodAgentTree root)
+    {
+        _root = root;
+    }
+    
+    public override NodeState Evaluate()
+    {
+        _root.Rest();
+        
+        _state = NodeState.RUNNING;
+        return _state;
+    }
+}
