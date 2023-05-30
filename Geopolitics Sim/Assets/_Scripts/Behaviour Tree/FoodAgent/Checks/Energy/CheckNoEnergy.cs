dd95fe3ed65551a52eb309f122db196a19c24922
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 01:05:20 +0200

    [ADD] Energy check nodes;

---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/Checks/Energy/CheckNoEnergy.cs
@@ -0,0 +1,16 @@
+﻿using BehaviorTree;
+
+public class CheckNoEnergy : Node
+{
+    public CheckNoEnergy(FoodAgentTree root)
+    {
+        _root = root;
+    }
+    
+    public override NodeState Evaluate()
+    {
+        _state = _root.EnergyLevel <= GlobalSettings.EnergyRestPoint ? NodeState.SUCCESS : NodeState.FAILURE;
+        
+        return _state;
+    }
+}
