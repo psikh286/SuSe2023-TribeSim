0ab20907da321df248d44302618643a0468aa52f
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 00:59:55 +0200

    [ADD] Inverter node;

---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/Core/Inverter.cs
@@ -0,0 +1,30 @@
+﻿using BehaviorTree;
+
+public class Inverter : Node
+{
+    private readonly Node _child;
+    
+    public Inverter(Node child)
+    {
+        _child = child;
+    }
+    
+    public override NodeState Evaluate()
+    {
+        switch (_child.Evaluate())
+        {
+            case NodeState.RUNNING:
+                _state = NodeState.RUNNING;
+                return _state;
+            case NodeState.SUCCESS:
+                _state = NodeState.FAILURE;
+                return _state;
+            case NodeState.FAILURE:
+                _state = NodeState.SUCCESS;
+                return _state;
+            default:
+                _state = NodeState.RUNNING;
+                return _state;
+        }
+    }
+}
