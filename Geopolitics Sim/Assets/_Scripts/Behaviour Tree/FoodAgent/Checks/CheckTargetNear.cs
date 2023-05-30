448b07045cb1fc6a8e1c72862b309d7ec9abe808
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 01:05:42 +0200

    [ADD] other check nodes;

---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/Checks/CheckTargetNear.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using BehaviorTree;
+
+public class CheckTargetNear: Node
+{
+    public CheckTargetNear(FoodAgentTree root)
+    {
+        _root = root;
+    }
+    
+    public override NodeState Evaluate()
+    {
+        var target = _root.Target;
+        
+        if (target == null)
+        {
+            _state = NodeState.FAILURE;
+            return _state;
+        }
+
+        _state = Vector3.Distance(_root.transform.position, target.position) > 0.01f ? NodeState.FAILURE : NodeState.SUCCESS;
+
+        return _state;
+    }
+}
