2f8c4ba49f5d263877f977e1a7cb04931fff0008
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 01:04:15 +0200

    [ADD] Water task nodes;

---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/Tasks/Water/TaskCollectWater.cs
@@ -0,0 +1,21 @@
+﻿using BehaviorTree;
+
+public class TaskCollectWater : Node
+{
+    public TaskCollectWater(FoodAgentTree root)
+    {
+        _root = root;
+    }
+
+    public override NodeState Evaluate()
+    {
+        _root.Water.Collect();
+        _root.IncreaseFoodCount();
+        
+        _root.Food = null;
+        _root.Target = null;
+        
+        _state = NodeState.RUNNING;
+        return _state;
+    }
+}
---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/Tasks/Water/TaskDrinkWater.cs
@@ -0,0 +1,17 @@
+﻿using BehaviorTree;
+
+public class TaskDrinkWater : Node
+{
+    public TaskDrinkWater(FoodAgentTree root)
+    {
+        _root = root;
+    }
+    
+    public override NodeState Evaluate()
+    {
+        _root.DrinkWater();
+        
+        _state = NodeState.RUNNING;
+        return _state;
+    }
+}
