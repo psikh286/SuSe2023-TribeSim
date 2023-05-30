c422df3fdd61809a63c48258fb63fc936a4e240d
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 01:08:14 +0200

    [ADD] settings to GlobalSettings;

--- Geopolitics Sim/Assets/_Scripts/Settings/GlobalSettings.cs
+++ Geopolitics Sim/Assets/_Scripts/Settings/GlobalSettings.cs
@@ -18,4 +18,50 @@
     public static float MutationMultiplier { get; private set; }
 
     public static void SetMutationMultiplier(float f) => MutationMultiplier = f;
+
+    #region Food
+
+    public static float IsHungryPercent { get; private set; }
+    public static void SetIsHungryPercent(float f) => IsHungryPercent = f;
+    
+    public static float FoodRegain { get; private set; }
+    public static void SetFoodRegain(float f) => FoodRegain = f;
+
+    #endregion
+
+    #region Water
+
+    public static float IsThirstyPercent { get; private set; }
+    public static void SetIsThirstyPercent(float f) => IsThirstyPercent = f;
+    
+    public static float WaterRegain { get; private set; }
+    public static void SetWaterRegain(float f) => WaterRegain = f;
+
+    #endregion
+
+    #region Energy
+
+    public static float EnergyRestPoint { get; private set; }
+    public static void SetEnergyRestPoint(float f) => EnergyRestPoint = f;
+    
+    public static float EnergyRegain { get; private set; }
+    public static void SetEnergyRegain(float f) => EnergyRegain = f;
+    
+   
+
+    #endregion
+
+    #region DayAndNight
+
+    public static TimeOfDay Time { get; private set; }
+    public static void SetTimeOfTheDay(TimeOfDay time) => Time = time;
+
+    #endregion
+
+    #region Memory
+    
+    public static int MaxPositionMemory { get; private set; }
+    public static void SetMaxPositionMemory(int i) => MaxPositionMemory = i;
+
+    #endregion
 }
