4f2e83d7607dc835fc9c9ee97f77909a9a3131ae
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 01:04:44 +0200

    [ADD] Day and Night check nodes;

---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/Checks/DayAndNight/CheckIsDay.cs
@@ -0,0 +1,16 @@
+﻿using BehaviorTree;
+
+public class CheckIsDay : Node
+{
+    public CheckIsDay(FoodAgentTree root)
+    {
+        _root = root;
+    }
+    
+    public override NodeState Evaluate()
+    {
+        _state = GlobalSettings.Time == TimeOfDay.Day ? NodeState.SUCCESS : NodeState.FAILURE;
+        
+        return _state;
+    }
+}
