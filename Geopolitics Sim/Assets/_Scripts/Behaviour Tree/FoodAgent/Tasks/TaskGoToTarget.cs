fbeda9b872f779dcb01876efa31cb4310fb22b23
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 01:06:28 +0200

    [ADD & MOVE] other task nodes;

---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/Tasks/TaskGoToTarget.cs
@@ -0,0 +1,26 @@
+﻿using BehaviorTree;
+using UnityEngine;
+
+public class TaskGoToTarget : Node
+{
+    public TaskGoToTarget(FoodAgentTree root)
+    {
+        _root = root;
+    }
+
+    public override NodeState Evaluate()
+    {
+        var agentPosition = _root.transform.position;
+        var target = _root.Target;
+        var speed = _root.Speed;
+        
+        if (Vector3.Distance(agentPosition, target.position) > 0.01f)
+        {
+            var targetPosition = target.position;
+            _root.transform.position = Vector3.MoveTowards(agentPosition, targetPosition, speed * Time.deltaTime);
+        }
+
+        _state = NodeState.RUNNING;
+        return _state;
+    }
+}
