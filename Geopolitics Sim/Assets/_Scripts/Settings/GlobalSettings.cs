edf470ce0fa6ec4243cc6b7e2471532d7290f4fc
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 03:28:40 +0200

    [EDIT] GlobalSettings; [EDIT] SettingsSO; [ADD] Tooltips to settingsSo;

--- Geopolitics Sim/Assets/_Scripts/Settings/GlobalSettings.cs
+++ Geopolitics Sim/Assets/_Scripts/Settings/GlobalSettings.cs
@@ -1,41 +1,42 @@
 ﻿public static class GlobalSettings
 {
-    public static int FoodToRep { get; private set; }
-    public static int FoodToSurvive { get; private set; }
-    
-    public static void SetFoodToRep(int i) => FoodToRep = i;
-    public static void SetFoodToSurvive(int i) => FoodToSurvive = i;
-    
-    
-    
-    public static float MinSpeed { get; private set; }
-    public static float MaxSpeed { get; private set; }
+    #region Inheretance
 
+    public static float MinSpeed { get; private set; }
     public static void SetMinSpeed(float i) => MinSpeed = i;
+    
+    public static float MaxSpeed { get; private set; }
     public static void SetMaxSpeed(float i) => MaxSpeed = i;
 
 
     public static float MutationMultiplier { get; private set; }
-
     public static void SetMutationMultiplier(float f) => MutationMultiplier = f;
 
+    #endregion
+    
     #region Food
 
-    public static float IsHungryPercent { get; private set; }
-    public static void SetIsHungryPercent(float f) => IsHungryPercent = f;
+    public static float IsHungryThreshold { get; private set; }
+    public static void SetIsHungryThreshold(float f) => IsHungryThreshold = f;
     
     public static float FoodRegain { get; private set; }
     public static void SetFoodRegain(float f) => FoodRegain = f;
-
+    
+    public static int FoodToRep { get; private set; }
+    public static void SetFoodToRep(int i) => FoodToRep = i;
+    
     #endregion
 
     #region Water
 
-    public static float IsThirstyPercent { get; private set; }
-    public static void SetIsThirstyPercent(float f) => IsThirstyPercent = f;
+    public static float IsThirstyThreshold { get; private set; }
+    public static void SetIsThirstyThreshold(float f) => IsThirstyThreshold = f;
     
     public static float WaterRegain { get; private set; }
     public static void SetWaterRegain(float f) => WaterRegain = f;
+    
+    public static int WaterToRep { get; private set; }
+    public static void SetWaterToRep(int i) => WaterToRep = i;
 
     #endregion
 
@@ -44,8 +45,8 @@
     public static float EnergyRestPoint { get; private set; }
     public static void SetEnergyRestPoint(float f) => EnergyRestPoint = f;
     
-    public static float EnergyRegain { get; private set; }
-    public static void SetEnergyRegain(float f) => EnergyRegain = f;
+    public static float RestRegain { get; private set; }
+    public static void SetRestRegain(float f) => RestRegain = f;
     
    
 
--- Geopolitics Sim/Assets/_Scripts/Settings/SettingsSO.cs
+++ Geopolitics Sim/Assets/_Scripts/Settings/SettingsSO.cs
@@ -3,12 +3,30 @@
 [CreateAssetMenu(fileName = "Globals", menuName = "GlobalSettings")]
 public class SettingsSO : ScriptableObject
 {
-    [SerializeField] private int _foodToRep;
-    [SerializeField] private int _foodToSurvive;
-
+    [Header("Food")]
+    [SerializeField] [Tooltip("Food to reproduce")]private int _foodToRep;
+    [SerializeField] [Tooltip("Amount of hunger restored by consuming one food item")] private float _foodRegain;
+    [SerializeField] [Tooltip("Hunger threshold for food consumption")] private float _isHungryThreshold;
+    
+    
+    [Header("Water")]
+    [SerializeField] [Tooltip("Water to reproduce")]private int _waterToRep;
+    [SerializeField] [Tooltip("Amount of thirstiness restored by consuming one water item")] private float _waterRegain;
+    [SerializeField] [Tooltip("Thirstiness threshold for water consumption")] private float _isThirstyThreshold;
+    
+    [Header("Energy")]
+    [SerializeField] [Tooltip("Energy level <= rest point: stop moving and rest")] private float _restPoint;
+    [SerializeField] [Tooltip("Amount of energy restored by standing still during day")] private float _restRegain;
+    
+    [Header("Day And Night")]
+    [SerializeField] private TimeOfDay _time;
+    
+    [Header("Memory")]
+    [SerializeField] [Tooltip("Number of memory slots in the position array")] private int _posMemory;
+    
+    [Header("Inheritance")]
     [SerializeField] private float _minSpeed;
     [SerializeField] private float _maxSpeed;
-
     [SerializeField] private float _mutationMultiplier;
 
     private void OnEnable()
@@ -18,8 +36,27 @@ public class SettingsSO : ScriptableObject
 
     public void OnValidate()
     {
+        /*FOOD*/
         GlobalSettings.SetFoodToRep(_foodToRep);
-        GlobalSettings.SetFoodToSurvive(_foodToSurvive);
+        GlobalSettings.SetFoodRegain(_foodRegain);
+        GlobalSettings.SetIsHungryThreshold(_isHungryThreshold);
+        
+        /*WATER*/
+        GlobalSettings.SetWaterToRep(_waterToRep);
+        GlobalSettings.SetWaterRegain(_waterRegain);
+        GlobalSettings.SetIsThirstyThreshold(_isThirstyThreshold);
+        
+        /*ENERGY*/
+        GlobalSettings.SetEnergyRestPoint(_restPoint);
+        GlobalSettings.SetRestRegain(_restRegain);
+        
+        /*TIME*/
+        GlobalSettings.SetTimeOfTheDay(_time);
+        
+        /*MEMORY*/
+        GlobalSettings.SetMaxPositionMemory(_posMemory);
+        
+        /*INHERITANCE*/
         GlobalSettings.SetMinSpeed(_minSpeed);
         GlobalSettings.SetMaxSpeed(_maxSpeed);
         GlobalSettings.SetMutationMultiplier(_mutationMultiplier);
