136b307726db5c434b361547ab2f0fd9b08df6de
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 03:30:45 +0200

    [ADD] Reproduction task nodes;

---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/Tasks/Reproduction/TaskReproduce.cs
@@ -0,0 +1,32 @@
+﻿using BehaviorTree;
+using UnityEngine;
+
+public class TaskReproduce : Node
+{
+    public TaskReproduce(FoodAgentTree root)
+    {
+        _root = root;
+    }
+    
+    public override NodeState Evaluate()
+    {
+        var mate = _root.Mate;
+        var speed = CalculateSpeed(_root.Speed, mate.Speed);
+        
+        Object.Instantiate(_root, _root.transform.position, Quaternion.identity).Init(speed);
+
+        mate.ReproductionCost();
+        _root.ReproductionCost();
+        
+        _state = NodeState.RUNNING;
+        return _state;
+    }
+
+    private static float CalculateSpeed(float a, float b)
+    {
+        var c = (a + b) * 0.5f + Utility.RandomFloat() * GlobalSettings.MutationMultiplier * Utility.RandomInt(-1, 2);
+        c = Mathf.Clamp(c ,GlobalSettings.MinSpeed, GlobalSettings.MaxSpeed);
+        
+        return c;
+    }
+}
---
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/Tasks/Reproduction/TaskRequestMate.cs
@@ -0,0 +1,23 @@
+﻿using BehaviorTree;
+
+public class TaskRequestMate : Node
+{
+    public TaskRequestMate(FoodAgentTree root)
+    {
+        _root = root;
+    }
+    
+    public override NodeState Evaluate()
+    {
+        var accepted = _root.Mate.RequestMate(_root);
+
+        if (!accepted)
+        {
+            _root.SetMate(null);
+            _root.SetTarget(null);
+        }
+        
+        _state = NodeState.RUNNING;
+        return _state;
+    }
+}
