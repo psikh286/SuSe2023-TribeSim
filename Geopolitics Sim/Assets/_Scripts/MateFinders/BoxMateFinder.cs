c840b21912337adfac460b125eb62ffe9d749923
Alan Berberov <alan.almaco67@gmail.com>
2023-05-31 03:29:31 +0200

    [ADD] BoxMateFinder : IMateFinder;

---
+++ Geopolitics Sim/Assets/_Scripts/MateFinders/BoxMateFinder.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class BoxMateFinder : MonoBehaviour, IMateFinder
+{
+    [SerializeField] private Vector3 _halfSize;
+    [SerializeField] private int _maxTargets;
+    [SerializeField] private bool _drawGizmos;
+    
+    public FoodAgentTree FindMate()
+    {
+        var hitColliders = new Collider[_maxTargets];
+        var i = Physics.OverlapBoxNonAlloc(transform.position, _halfSize, hitColliders, Quaternion.identity, 1 << 8);
+
+        if (i <= 0) return null;
+        
+        var col = GetClosestCollider();
+
+        return col.TryGetComponent<FoodAgentTree>(out var mate) ? mate : null;
+        
+        Collider GetClosestCollider()
+        {
+            var bestDistance = float.MaxValue;
+            var pos = transform.position;
+            Collider bestCollider = null;
+
+            for (var j = 0; j < i; j++)
+            {
+                var distance = Vector3.Distance(pos, hitColliders[j].transform.position);
+
+                if (distance > bestDistance) continue;
+                
+                bestDistance = distance;
+                bestCollider = hitColliders[j];
+            }
+ 
+            return bestCollider;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(!_drawGizmos) return;
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireCube(transform.position, _halfSize * 2f);
+    }
+}
