d5a274b7297e306ae80279d7355d7a102155bd33
Alan Berberov <alan.almaco67@gmail.com>
2023-06-06 13:16:16 +0200

    [ADD] new static class for cooldown;

---
+++ Geopolitics Sim/Assets/_Scripts/Settings/CooldownConfiguration.cs
@@ -0,0 +1,10 @@
+﻿public static class CooldownConfiguration
+{
+    public static int RequestMate;
+    public static int EatFood;
+    public static int CollectFood = 100;
+    public static int DrinkWater;
+    public static int CollectWater = 100;
+    public static int Reproduce = 400;
+
+}
