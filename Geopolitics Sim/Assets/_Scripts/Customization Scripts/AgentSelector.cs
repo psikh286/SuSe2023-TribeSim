d6bd117b54d4b3f47d904762864673d5c7f0817d
Alan Berberov <alan.almaco67@gmail.com>
2023-06-20 23:57:55 +0200

    [REFACTOR] Agent selector;

--- Geopolitics Sim/Assets/_Scripts/Customization Scripts/AgentSelector.cs
+++ Geopolitics Sim/Assets/_Scripts/Customization Scripts/AgentSelector.cs
@@ -1,41 +1,46 @@
+using System;
 using UnityEngine;
 
 public class AgentSelector : MonoBehaviour
 {
-    [HideInInspector]
     public GameObject selectedAgent; // The currently selected agent
+
+    public static Action<bool, GameObject> OnAgentStatusChanged;
+
+    private Camera _cam;
+
+    private void Awake()
+    {
+        _cam = Camera.main;
+    }
 
     private void Update()
     {
         // Check if the left mouse button was clicked
-        if (Input.GetMouseButtonDown(0))
+        if (!Input.GetMouseButtonDown(0)) return;
+        
+        // Create a ray from the mouse cursor on screen in the direction of the camera
+        var ray = _cam.ScreenPointToRay(Input.mousePosition);
+
+        // Perform the raycast
+        if (!Physics.Raycast(ray, out var hit, 100, 1 << 8)) return;
+        
+        // Deselect the currently selected agent, if there is one
+        if (selectedAgent != null)
         {
-            // Create a ray from the mouse cursor on screen in the direction of the camera
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            // Perform the raycast
-            if (Physics.Raycast(ray, out hit))
-            {
-                // Check if an agent was clicked
-                if (hit.transform.CompareTag("Agent"))
-                {
-                    // Deselect the currently selected agent, if there is one
-                    if (selectedAgent != null)
-                    {
-                        selectedAgent.GetComponent<randomCharacter>().Deselect();
-                    }
-
-                    // Select the new agent
-                    selectedAgent = hit.transform.gameObject;
-                    selectedAgent.GetComponent<randomCharacter>().Select();
-                }
-            }
+            selectedAgent.GetComponent<randomCharacter>().Deselect();
+            OnAgentStatusChanged?.Invoke(false, selectedAgent);
         }
+
+        // Select the new agent
+        selectedAgent = hit.transform.gameObject;
+        selectedAgent.GetComponent<randomCharacter>().Select();
+        OnAgentStatusChanged?.Invoke(true, selectedAgent);
     }
 
     public void TriggerDeselect()
     {
         selectedAgent.GetComponent<randomCharacter>().Deselect();
+        OnAgentStatusChanged?.Invoke(false, selectedAgent);
     }
 }
