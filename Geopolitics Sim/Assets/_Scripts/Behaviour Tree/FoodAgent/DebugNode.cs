fb2ad44da8c047e6ca69b18f89c85c48924ee2e4
Alan Berberov <alan.almaco67@gmail.com>
2023-06-20 22:40:58 +0200

    [EDIT] DebugNode;

--- Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/DebugNode.cs
+++ Geopolitics Sim/Assets/_Scripts/Behaviour Tree/FoodAgent/DebugNode.cs
@@ -1,4 +1,20 @@
-﻿public class DebugNode
+﻿using BehaviorTree;
+
+public class DebugNode : Node
 {
+    private readonly string _message;
     
+    public DebugNode(FoodAgentTree root, string message)
+    {
+        _root = root;
+        _message = message;
+    }
+    
+    public override NodeState Evaluate()
+    {
+        _root.OnAgentAct?.Invoke(_message);
+
+        _state = NodeState.SUCCESS;
+        return _state;
+    }
 }
